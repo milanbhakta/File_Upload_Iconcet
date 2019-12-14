bf0879aac300fff7a54394e237c1b126ec8502e4
Milan Bhakta <milzbhakta@gmail.com>
2019-12-13 21:55:17 -0500

    Push

---
+++ client/build/precache-manifest.ff9b295fa22bac869442d1b438979987.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "18cfc9e077602b7973e8",
+    "url": "/milanbhakta/File_Upload_Iconcet/static/js/runtime~main.324bcd0a.js"
+  },
+  {
+    "revision": "fa97e2b3393a7b66b891",
+    "url": "/milanbhakta/File_Upload_Iconcet/static/js/main.acbb50c1.chunk.js"
+  },
+  {
+    "revision": "27c43ddf2cf90881e399",
+    "url": "/milanbhakta/File_Upload_Iconcet/static/js/2.c59b9399.chunk.js"
+  },
+  {
+    "revision": "fa97e2b3393a7b66b891",
+    "url": "/milanbhakta/File_Upload_Iconcet/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "cce74b4ac8fd7fb1585f1c3fe9b3042f",
+    "url": "/milanbhakta/File_Upload_Iconcet/index.html"
+  }
+];
